ea11c2e43808ff466e72e245c325b9aff69e57ab
Pat Wilson <zerostride@gmail.com>
2016-07-14 11:22:34 -0700

    Fixed up signed requests.

--- Teak/TeakRequest.m
+++ Teak/TeakRequest.m
@@ -27,7 +27,7 @@
 @interface TeakRequest ()
 @property (strong, nonatomic, readwrite) NSString* endpoint;
 @property (strong, nonatomic, readwrite) NSDictionary* payload;
-@property (strong, nonatomic, readwrite) TeakRequestResponse callback;
+@property (copy, nonatomic, readwrite) TeakRequestResponse callback;
 @property (strong, nonatomic) NSString* hostname;
 @property (strong, nonatomic) NSURLSession* urlSession;
 @property (strong, nonatomic) NSMutableData* receivedData;
@@ -39,104 +39,127 @@
    self = [super init];
    if (self) {
       self.endpoint = endpoint;
-      self.callback = [callback copy];
-      self.hostname = @"gocarrot.com"; // HAX, TODO: Support other hostnames via RemoteConfiguration
+      self.callback = callback;
+      self.hostname = @"gocarrot.com"; // TODO: Support other hostnames via RemoteConfiguration
       self.urlSession = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]
                                                       delegate:self
                                                  delegateQueue:nil];
       self.receivedData = [[NSMutableData alloc] init];
       [self.receivedData setLength:0];
 
-      NSMutableDictionary* payloadWithCommon = [NSMutableDictionary dictionaryWithDictionary:payload];
-      [payloadWithCommon addEntriesFromDictionary:@{
-         @"appstore_name" : @"apple",
-         @"game_id" : session.appConfiguration.appId,
-         @"sdk_version" : [Teak sharedInstance].sdkVersion,
-         @"sdk_platform" : session.deviceConfiguration.platformString,
-         @"app_version" : session.appConfiguration.appVersion,
-         @"device_model" : session.deviceConfiguration.deviceModel,
-         @"bundle_id" : session.appConfiguration.bundleId,
-         @"device_id" : session.deviceConfiguration.deviceId,
-         @"is_sandbox" : [NSNumber numberWithBool:!session.appConfiguration.isProduction]
-      }];
-      self.payload = [self signedPayload:payloadWithCommon withSession:session];
+      @try {
+         NSMutableDictionary* payloadWithCommon = [NSMutableDictionary dictionaryWithDictionary:payload];
+         [payloadWithCommon addEntriesFromDictionary:@{
+            @"appstore_name" : @"apple",
+            @"game_id" : session.appConfiguration.appId,
+            @"sdk_version" : [Teak sharedInstance].sdkVersion,
+            @"sdk_platform" : session.deviceConfiguration.platformString,
+            @"app_version" : session.appConfiguration.appVersion,
+            @"device_model" : session.deviceConfiguration.deviceModel,
+            @"bundle_id" : session.appConfiguration.bundleId,
+            @"device_id" : session.deviceConfiguration.deviceId,
+            @"is_sandbox" : [NSNumber numberWithBool:!session.appConfiguration.isProduction]
+         }];
+         if (session.userId) {
+            [payloadWithCommon setObject:session.userId forKey:@"api_key"];
+         }
+         self.payload = [self signedPayload:payloadWithCommon withSession:session];
+      } @catch (NSException* exception) {
+         TeakLog(@"Error creating request payload. %@", exception);
+         return nil;
+      }
    }
    return self;
 }
 
-
 - (nonnull NSDictionary*)signedPayload:(nonnull NSDictionary*)payloadToSign withSession:(nonnull TeakSession*)session {
-   NSString* path = self.endpoint;
-   if(path == nil || path.length < 1) path = @"/";
+   @try {
+      NSString* path = self.endpoint;
+      if(path == nil || path.length < 1) path = @"/";
 
-   // Build query string to sign
-   NSArray* queryKeysSorted = [[payloadToSign allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
-   NSMutableString* sortedQueryString = [[NSMutableString alloc] init];
-   for (int i = 0; i < queryKeysSorted.count; i++) {
-      NSString* key = [queryKeysSorted objectAtIndex:i];
-      id value = [payloadToSign objectForKey:key];
+      // Build query string to sign
+      NSArray* queryKeysSorted = [[payloadToSign allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)];
+      NSMutableString* sortedQueryString = [[NSMutableString alloc] init];
+      for (int i = 0; i < queryKeysSorted.count; i++) {
+         NSString* key = [queryKeysSorted objectAtIndex:i];
+         id value = [payloadToSign objectForKey:key];
 
-      NSString* valueString = value;
-      if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) {
-         NSError* error = nil;
-         NSData* jsonData = [NSJSONSerialization dataWithJSONObject:value options:0 error:&error];
-         if (error) {
-            TeakLog(@"Error converting %@ to JSON: %@", value, error);
-            valueString = [value description];
-         } else {
-            valueString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+         NSString* valueString = value;
+         if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) {
+            NSError* error = nil;
+            NSData* jsonData = [NSJSONSerialization dataWithJSONObject:value options:0 error:&error];
+            if (error) {
+               TeakLog(@"Error converting %@ to JSON: %@", value, error);
+               valueString = [value description];
+            } else {
+               valueString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            }
          }
+         [sortedQueryString appendFormat:@"%@=%@%s", key, valueString, (i + 1 < queryKeysSorted.count ? "&" : "")];
       }
-      [sortedQueryString appendFormat:@"%@=%@%s", key, valueString, (i + 1 < queryKeysSorted.count ? "&" : "")];
-   }
 
-   NSString* stringToSign = [NSString stringWithFormat:@"%@\n%@\n%@\n%@", @"POST", self.hostname, path, sortedQueryString];
+      NSString* stringToSign = [NSString stringWithFormat:@"%@\n%@\n%@\n%@", @"POST", self.hostname, path, sortedQueryString];
 
-   NSData* dataToSign = [stringToSign dataUsingEncoding:NSUTF8StringEncoding];
-   uint8_t digestBytes[CC_SHA256_DIGEST_LENGTH];
-   CCHmac(kCCHmacAlgSHA256, [session.appConfiguration.apiKey UTF8String], session.appConfiguration.apiKey.length, [dataToSign bytes], [dataToSign length], &digestBytes);
+      NSData* dataToSign = [stringToSign dataUsingEncoding:NSUTF8StringEncoding];
+      uint8_t digestBytes[CC_SHA256_DIGEST_LENGTH];
+      CCHmac(kCCHmacAlgSHA256, [session.appConfiguration.apiKey UTF8String], session.appConfiguration.apiKey.length, [dataToSign bytes], [dataToSign length], &digestBytes);
 
-   NSData* digestData = [NSData dataWithBytes:digestBytes length:CC_SHA256_DIGEST_LENGTH];
-   NSString* sigString = [digestData base64EncodedStringWithOptions:0];
+      NSData* digestData = [NSData dataWithBytes:digestBytes length:CC_SHA256_DIGEST_LENGTH];
+      NSString* sigString = [digestData base64EncodedStringWithOptions:0];
 
-   // Build params dictionary with JSON object representations
-   NSMutableDictionary* retParams = [[NSMutableDictionary alloc] init];
-   for (int i = 0; i < queryKeysSorted.count; i++) {
-      NSString* key = [queryKeysSorted objectAtIndex:i];
-      id value = [payloadToSign objectForKey:key];
-      NSString* valueString = value;
-      if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) {
-         NSError* error = nil;
-         NSData* jsonData = [NSJSONSerialization dataWithJSONObject:value options:0 error:&error];
+      // Build params dictionary with JSON object representations
+      NSMutableDictionary* retParams = [[NSMutableDictionary alloc] init];
+      for (int i = 0; i < queryKeysSorted.count; i++) {
+         NSString* key = [queryKeysSorted objectAtIndex:i];
+         id value = [payloadToSign objectForKey:key];
+         NSString* valueString = value;
+         if ([value isKindOfClass:[NSDictionary class]] || [value isKindOfClass:[NSArray class]]) {
+            NSError* error = nil;
+            NSData* jsonData = [NSJSONSerialization dataWithJSONObject:value options:0 error:&error];
 
-         if (error) {
-            TeakLog(@"Error converting %@ to JSON: %@", value, error);
-            valueString = [value description];
-         } else {
-            valueString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            if (error) {
+               TeakLog(@"Error converting %@ to JSON: %@", value, error);
+               valueString = [value description];
+            } else {
+               valueString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+            }
          }
+         [retParams setObject:valueString forKey:key];
       }
-      [retParams setObject:valueString forKey:key];
-   }
-   [retParams setObject:sigString forKey:@"sig"];
+      [retParams setObject:sigString forKey:@"sig"];
 
-   return retParams;
+      return retParams;
+   } @catch (NSException* exception) {
+      TeakLog(@"Error while signing parameters. %@", exception);
+      return payloadToSign;
+   }
 }
 
 - (void)send {
-   NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://%@%@", self.hostname, self.endpoint]]];
-   NSData* payloadData = [NSJSONSerialization dataWithJSONObject:self.payload options:0 error:nil];
+   @try {
+      NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"https://%@%@", self.hostname, self.endpoint]]];
 
-   [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-   [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
-   //[request setValue:@"gzip" forHTTPHeaderField:@"Content-Encoding"]; // TODO: gzip?
-   [request setValue:@"teak" forHTTPHeaderField:@"User-Agent"];
-   [request setHTTPMethod:@"POST"];
-   [request setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[payloadData length]] forHTTPHeaderField:@"Content-Length"];
-   [request setHTTPBody:payloadData];
+      NSString* boundry = @"-===-httpB0unDarY-==-";
+      
+      NSMutableData* postData = [[NSMutableData alloc] init];
+      
+      for (NSString* key in self.payload) {
+         [postData appendData:[[NSString stringWithFormat:@"--%@\r\n", boundry] dataUsingEncoding:NSUTF8StringEncoding]];
+         [postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n%@\r\n", key,[self.payload objectForKey:key]] dataUsingEncoding:NSUTF8StringEncoding]];
+      }
+      [postData appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundry] dataUsingEncoding:NSUTF8StringEncoding]];
 
-   NSURLSessionDataTask *dataTask = [self.urlSession dataTaskWithRequest:request];
-   [dataTask resume];
+      [request setHTTPMethod:@"POST"];
+      [request setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[postData length]] forHTTPHeaderField:@"Content-Length"];
+      [request setHTTPBody:postData];
+      NSString* charset = (NSString*)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+      [request setValue:[NSString stringWithFormat:@"multipart/form-data; charset=%@; boundary=%@", charset, boundry] forHTTPHeaderField:@"Content-Type"];
+
+      NSURLSessionDataTask *dataTask = [self.urlSession dataTaskWithRequest:request];
+      [dataTask resume];
+   } @catch(NSException* exception) {
+      TeakLog(@"Error sending request. %@", exception);
+   }
 }
 
 - (void)URLSession:(NSURLSession*)session dataTask:(NSURLSessionDataTask*)dataTask didReceiveResponse:(NSURLResponse*)response completionHandler:(void (^)(NSURLSessionResponseDisposition disposition))completionHandler {
@@ -145,7 +168,11 @@
 }
 
 - (void)URLSession:(NSURLSession*)session dataTask:(NSURLSessionDataTask*)dataTask didReceiveData:(NSData*)data {
-   [self.receivedData appendData:data];
+   @try {
+      [self.receivedData appendData:data];
+   } @catch(NSException* exception) {
+      TeakLog(@"Error appending data from URLSession. %@", exception);
+   }
 }
 
 - (void)URLSession:(NSURLSession*)session task:(NSURLSessionTask*)task didCompleteWithError:(NSError*)error {
@@ -153,8 +180,15 @@
       // TODO: Handle error
    }
    else {
-      NSDictionary* reply = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:self.receivedData options:kNilOptions error:&error];
-      self.callback(task.response, reply);
+      @try {
+         NSDictionary* reply = (NSDictionary*)[NSJSONSerialization JSONObjectWithData:self.receivedData options:kNilOptions error:&error];
+
+         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+            self.callback(task.response, reply);
+         });
+      } @catch(NSException* exception) {
+         TeakLog(@"Error deserializing JSON reply. %@", exception);
+      }
    }
 }
 
