a773a9d331bea896b3e2435597a56b9e84eecf19
Pat Wilson <zerostride@gmail.com>
2017-01-05 15:32:32 -0800

    Removed cruft, and added tutorial steps and Apache 2.0 copyright.

--- Sample/Sample/AppDelegate.m
+++ Sample/Sample/AppDelegate.m
@@ -1,12 +1,22 @@
-//
-//  AppDelegate.m
-//  Sample
-//
-//  Created by Pat Wilson on 2/24/16.
-//  Copyright © 2016 GoCarrot Inc. All rights reserved.
-//
+/* Teak Example -- Copyright (C) 2016 GoCarrot Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "AppDelegate.h"
+
+// Step 3:
+// Import Teak into your UIApplicationDelegate implementation.
 #import <Teak/Teak.h>
 
 @import AdSupport;
@@ -17,16 +27,28 @@
 
 @implementation AppDelegate
 
-- (void)application:(UIApplication*)application handleEventsForBackgroundURLSession:(NSString*)identifier completionHandler:(void (^)())completionHandler
-{
-   NSLog(@"application:handleEventsForBackgroundURLSession:completionHandler: - Session Identifier: %@", identifier);
-}
-
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+   // For this example, we are simply using the IDFA for a user id. In your game, you will
+   // want to use the same user id that you use in your database.
+   //
+   // These user ids should be unique, no two players should have the same user id.
    NSString* userId = [[ASIdentifierManager sharedManager].advertisingIdentifier UUIDString];
+
+   // Step 3:
+   // Call identifyUser as soon as you know the user id of the current player.
    [[Teak sharedInstance] identifyUser:userId];
 
+   // Step 4:
+   // Tell Teak that you want to be notified when your game has been launched via a Push Notification.
+   //
+   // See the bottom of this file for an example of a handler function.
+   [[NSNotificationCenter defaultCenter] addObserver:self
+                                            selector:@selector(handleTeakNotification:)
+                                                name:TeakNotificationAppLaunch
+                                              object:nil];
+
+   // The following code registers for push notifications in both an iOS 8 and iOS 9+ friendly way
    if ([application respondsToSelector:@selector(registerUserNotificationSettings:)]) {
       UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeAlert | UIUserNotificationTypeBadge | UIUserNotificationTypeSound) categories:nil];
       [application registerUserNotificationSettings:settings];
@@ -38,66 +60,27 @@
 #pragma clang diagnostic pop
    }
 
-   [[NSNotificationCenter defaultCenter] addObserver:self
-                                            selector:@selector(handleTeakNotification:)
-                                                name:TeakNotificationAppLaunch
-                                              object:nil];
-/*
-   NSArray* locales = [NSLocale preferredLanguages];
-   NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-   NSString *documentsDirectory = [paths objectAtIndex:0];
-   NSString* path = [documentsDirectory stringByAppendingPathComponent:@"myfile.csv"];
-   
-   [[NSFileManager defaultManager] createFileAtPath: path contents:nil attributes:nil];
-   NSFileHandle *handle = [NSFileHandle fileHandleForWritingAtPath:path];
-   
-   for(int i = 0; i < [locales count]; i++)
-   {
-      NSString* writeString = [NSString stringWithFormat:@"%@,%@\n",
-                               [locales objectAtIndex:i],
-                               [[NSLocale currentLocale] displayNameForKey:NSLocaleIdentifier value:[locales objectAtIndex:i]]];
-      [handle writeData:[writeString dataUsingEncoding:NSUTF8StringEncoding]];
-   }
-*/
-   
    return YES;
-}
-
-- (void)handleTeakNotification:(NSNotification*)notification
-{
-   NSDictionary* teakReward = [notification.userInfo objectForKey:@"teakReward"];
-   NSDictionary* teakDeepLinkPath = [notification.userInfo objectForKey:@"teakDeepLinkPath"];
-   NSDictionary* teakDeepLinkQueryParameters = [notification.userInfo objectForKey:@"teakDeepLinkQueryParameters"];
-   NSLog(@"TEAK TOLD US ABOUT A NOTIFICATION, THANKS TEAK!\n%@\n%@\n%@", teakReward, teakDeepLinkPath, teakDeepLinkQueryParameters);
 }
 
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings
 {
-   //register to receive notifications
+   // Register to receive notifications
    [application registerForRemoteNotifications];
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-   // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-   // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-   NSLog(@"applicationWillResignActive:");
-}
+// This is an example of a handler function that will be called when your app
+// is launched from a Push Notification.
+- (void)handleTeakNotification:(NSNotification*)notification
+{
+   // If the notification contains a reward, it will be found in the 'teakReward' key of the user info dictionary.
+   NSDictionary* teakReward = [notification.userInfo objectForKey:@"teakReward"];
 
-- (void)applicationDidEnterBackground:(UIApplication *)application {
+   // If the notification contains a deep link, the path of the link will be found in the 'teakDeepLinkPath' of the user info dictionary.
+   NSDictionary* teakDeepLinkPath = [notification.userInfo objectForKey:@"teakDeepLinkPath"];
+   NSDictionary* teakDeepLinkQueryParameters = [notification.userInfo objectForKey:@"teakDeepLinkQueryParameters"];
 
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-   // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-   NSLog(@"applicationWillEnterForeground:");
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-   
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-   // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+   NSLog(@"TEAK TOLD US ABOUT A NOTIFICATION, THANKS TEAK!\n%@\n%@\n%@", teakReward, teakDeepLinkPath, teakDeepLinkQueryParameters);
 }
 
 @end
--- Sample/Sample/main.m
+++ Sample/Sample/main.m
@@ -1,18 +1,35 @@
-//
-//  main.m
-//  Sample
-//
-//  Created by Pat Wilson on 2/24/16.
-//  Copyright © 2016 GoCarrot Inc. All rights reserved.
-//
+/* Teak Example -- Copyright (C) 2016 GoCarrot Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+
+// Step 1:
+// Import Teak into the main.m file to use the initialization method.
 #import <Teak/Teak.h>
 
 int main(int argc, char * argv[]) {
    @autoreleasepool {
-      [Teak initForApplicationId:@"1136371193060244" withClass:[AppDelegate class] andApiKey:@"1f3850f794b9093864a0778009744d03"];
+      // Step 2:
+      // Initialize Teak inside the @autoreleasepool but before UIApplicationMain() is called.
+      [Teak initForApplicationId:@"1136371193060244"                    // Use your Teak Application Id here.
+                       withClass:[AppDelegate class]                    // Use the name of your main UIApplicationDelegate here.
+                       andApiKey:@"1f3850f794b9093864a0778009744d03"];  // Use your Teak API Key here.
+
+      // Continue to our AppDelegate.m file for the next steps.
+
       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
    }
 }
