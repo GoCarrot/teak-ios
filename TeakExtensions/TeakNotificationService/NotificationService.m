45f45aa6f4a3da59ad177856a3ae847c2e5a4e54
Pat Wilson <zerostride@gmail.com>
2017-11-02 16:21:12 -0700

    Add copywrite before I forget

--- TeakExtensions/TeakNotificationService/NotificationService.m
+++ TeakExtensions/TeakNotificationService/NotificationService.m
@@ -1,18 +1,23 @@
-//
-//  NotificationService.m
-//  TeakNotificationService
-//
-//  Created by Pat Wilson on 11/1/17.
-//  Copyright © 2017 GoCarrot Inc. All rights reserved.
-//
+/* Teak -- Copyright (C) 2017 GoCarrot Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #import "NotificationService.h"
 
 @interface NotificationService ()
-
 @property (nonatomic, strong) void (^contentHandler)(UNNotificationContent* contentToDeliver);
 @property (nonatomic, strong) UNMutableNotificationContent* bestAttemptContent;
-
 @end
 
 @implementation NotificationService
@@ -39,15 +44,18 @@
   self.contentHandler = contentHandler;
   self.bestAttemptContent = [request.content mutableCopy];
 
-  // Modify the notification content here...
-  self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
-
-  self.contentHandler(self.bestAttemptContent);
+  @try {
+    NSDictionary* notification = request.content.userInfo[@"aps"];
+    NSString* teakNotifId = notification[@"teakNotifId"];
+    if ([teakNotifId length > 0]) {
+      self.bestAttemptContent.title = [NSString stringWithFormat:@"%@", teakNotifId];
+    }
+  } @finally {
+    self.contentHandler(self.bestAttemptContent);
+  }
 }
 
 - (void)serviceExtensionTimeWillExpire {
-  // Called just before the extension will be terminated by the system.
-  // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
   self.contentHandler(self.bestAttemptContent);
 }
 
