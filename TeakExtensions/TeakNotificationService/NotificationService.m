29290fa32823f77bffa93b5a2a9e3231b4e440e5
Pat Wilson <zerostride@gmail.com>
2017-11-03 14:49:56 -0700

    Report the notification_received metric.

--- TeakExtensions/TeakNotificationService/NotificationService.m
+++ TeakExtensions/TeakNotificationService/NotificationService.m
@@ -16,47 +16,88 @@
 #import "NotificationService.h"
 
 @interface NotificationService ()
-@property (nonatomic, strong) void (^contentHandler)(UNNotificationContent* contentToDeliver);
-@property (nonatomic, strong) UNMutableNotificationContent* bestAttemptContent;
+@property (strong, nonatomic) void (^contentHandler)(UNNotificationContent* contentToDeliver);
+@property (strong, nonatomic) UNMutableNotificationContent* bestAttemptContent;
+@property (strong, nonatomic) NSURLSession* session;
+
+- (NSURLRequest*)metricRequestForPayload:(NSDictionary*)payload;
 @end
+
+NSString* TeakNSStringOrNilFor(id object) {
+  if (object == nil) return nil;
+
+  NSString* ret = nil;
+  @try {
+    ret = ((object == nil || [object isKindOfClass:[NSString class]]) ? object : [object stringValue]);
+  } @catch (NSException* ignored) {
+  }
+  return ret;
+}
 
 @implementation NotificationService
 
-/*
- final String teakUserId = bundle.getString("teakUserId", null);
- if (teakUserId != null) {
- final Session session = Session.getCurrentSessionOrNull();
- final TeakConfiguration teakConfiguration = TeakConfiguration.get();
- if (session != null) {
- HashMap<String, Object> payload = new HashMap<>();
- payload.put("app_id", teakConfiguration.appConfiguration.appId);
- payload.put("user_id", teakUserId);
- payload.put("platform_id", teakNotification.teakNotifId);
- if (teakNotification.teakNotifId == 0) {
- payload.put("impression", false);
- }
- 
- asyncExecutor.execute(new Request("parsnip.gocarrot.com", "/notification_received", payload, session));
- }
- }
- */
 - (void)didReceiveNotificationRequest:(UNNotificationRequest*)request withContentHandler:(void (^)(UNNotificationContent* _Nonnull))contentHandler {
   self.contentHandler = contentHandler;
   self.bestAttemptContent = [request.content mutableCopy];
 
   @try {
     NSDictionary* notification = request.content.userInfo[@"aps"];
-    NSString* teakNotifId = notification[@"teakNotifId"];
-    if ([teakNotifId length > 0]) {
+    NSString* teakNotifId = TeakNSStringOrNilFor(notification[@"teakNotifId"]);
+    if ([teakNotifId length] > 0) {
+      // TODO: May want to use backgroundSession?
+      self.session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration ephemeralSessionConfiguration]];
+
+      // HACK FOR TESTING
       self.bestAttemptContent.title = [NSString stringWithFormat:@"%@", teakNotifId];
+
+      // Send notification_received metric
+      NSString* teakUserId = TeakNSStringOrNilFor(notification[@"teakUserId"]);
+      if ([teakUserId length] > 0) {
+        NSDictionary* payload = @{
+          @"user_id" : teakUserId,
+          @"platform_id" : teakNotifId,
+          @"network_id" : @3
+        };
+        [self sendMetricForPayload:payload];
+      }
     }
-  } @finally {
+  }
+  @finally {
     self.contentHandler(self.bestAttemptContent);
   }
 }
 
 - (void)serviceExtensionTimeWillExpire {
+  [self.session invalidateAndCancel];
   self.contentHandler(self.bestAttemptContent);
+}
+
+- (void)sendMetricForPayload:(NSDictionary*)payload {
+  NSMutableURLRequest* request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:@"https://parsnip.gocarrot.com/notification_received"]];
+
+  NSString* boundry = @"-===-httpB0unDarY-==-";
+
+  NSMutableData* postData = [[NSMutableData alloc] init];
+
+  for (NSString* key in payload) {
+    [postData appendData:[[NSString stringWithFormat:@"--%@\r\n", boundry] dataUsingEncoding:NSUTF8StringEncoding]];
+    [postData appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"%@\"\r\n\r\n%@\r\n", key, [payload objectForKey:key]] dataUsingEncoding:NSUTF8StringEncoding]];
+  }
+  [postData appendData:[[NSString stringWithFormat:@"--%@--\r\n", boundry] dataUsingEncoding:NSUTF8StringEncoding]];
+
+  [request setHTTPMethod:@"POST"];
+  [request setValue:[NSString stringWithFormat:@"%lu", (unsigned long)[postData length]] forHTTPHeaderField:@"Content-Length"];
+  [request setHTTPBody:postData];
+  NSString* charset = (NSString*)CFStringConvertEncodingToIANACharSetName(CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+  [request setValue:[NSString stringWithFormat:@"multipart/form-data; charset=%@; boundary=%@", charset, boundry] forHTTPHeaderField:@"Content-Type"];
+
+  NSURLSessionUploadTask* uploadTask =
+      [self.session uploadTaskWithRequest:request
+                                 fromData:nil
+                        completionHandler:^(NSData* data, NSURLResponse* response, NSError* error) {
+                          [self.session finishTasksAndInvalidate];
+                        }];
+  [uploadTask resume];
 }
 
 @end
