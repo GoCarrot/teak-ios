05b29a03f2340c5ba78eb664126d616ed08db5e7
Pat Wilson <zerostride@gmail.com>
2018-01-10 14:52:08 -0800

    Need CoreGraphics for CGImageRelease

--- TeakExtensions/add_teak_extensions.rb
+++ TeakExtensions/add_teak_extensions.rb
@@ -35,7 +35,7 @@ xcode_proj = Xcodeproj::Project.open(xcode_project_path)
 # List of Teak extensions
 teak_extensions = [
   ["TeakNotificationService", ["MobileCoreServices", "UserNotifications"]],
-  ["TeakNotificationContent", ["UserNotifications", "UserNotificationsUI", "AVFoundation", "UIKit", "ImageIO"]]
+  ["TeakNotificationContent", ["UserNotifications", "UserNotificationsUI", "AVFoundation", "UIKit", "ImageIO", "CoreGraphics"]]
 ]
 teak_extensions.each do |service, deps|
 
