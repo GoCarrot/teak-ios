cdc33397ba79ae54fffa4a1401eaef7f269c8410
Pat Wilson <pat@teak.io>
2019-06-14 12:42:07 -0700

    This needs to run canOpenURL: on the main thread

--- Teak/TeakSession.m
+++ Teak/TeakSession.m
@@ -366,21 +366,26 @@ DefineTeakState(Expired, (@[]));
   NSString* deepLink = self.launchAttribution[@"deep_link"];
 
   if (deepLink != nil) {
-    NSURL* url = [NSURL URLWithString:deepLink];
-    if (url != nil) {
-      // If there's a deep link, see if Teak handles it. Otherwise use openURL.
-      BOOL teakHandledDeepLink = TeakLink_HandleDeepLink(url);
-      UIApplication* application = [UIApplication sharedApplication];
+    @try {
+      NSURL* url = [NSURL URLWithString:deepLink];
+      if (url != nil) {
+        // If there's a deep link, see if Teak handles it. Otherwise use openURL.
+        BOOL teakHandledDeepLink = TeakLink_HandleDeepLink(url);
+        UIApplication* application = [UIApplication sharedApplication];
 
-      if (!teakHandledDeepLink && [application canOpenURL:url]) {
-        if (sHostAppOpenURLOptionsIMP) {
-          // iOS 10+
-          sHostAppOpenURLOptionsIMP(self, @selector(application:openURL:options:), application, url, [[NSDictionary alloc] init]);
-        } else if (sHostAppOpenURLIMP) {
-          // iOS < 10
-          sHostAppOpenURLIMP(self, @selector(application:openURL:sourceApplication:annotation:), application, url, [application description], nil);
-        }
+        dispatch_async(dispatch_get_main_queue(), ^{
+          if (!teakHandledDeepLink && [application canOpenURL:url]) {
+            if (sHostAppOpenURLOptionsIMP) {
+              // iOS 10+
+              sHostAppOpenURLOptionsIMP(self, @selector(application:openURL:options:), application, url, [[NSDictionary alloc] init]);
+            } else if (sHostAppOpenURLIMP) {
+              // iOS < 10
+              sHostAppOpenURLIMP(self, @selector(application:openURL:sourceApplication:annotation:), application, url, [application description], nil);
+            }
+          }
+        });
       }
+    } @finally {
     }
   }
 
