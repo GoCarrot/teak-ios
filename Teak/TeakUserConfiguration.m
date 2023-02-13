91be3b2b4d0ee3d051ccace29bdfa56ec05d390c
Ezri Coniglio <zerostride@gmail.com>
2023-02-13 13:31:28 -0800

    Ignore these deprecations as well

--- Teak/TeakUserConfiguration.m
+++ Teak/TeakUserConfiguration.m
@@ -6,7 +6,10 @@
   return @{
     @"email" : (self.email == nil ? [NSNull null] : self.email),
     @"facebook_id" : (self.facebookId == nil ? [NSNull null] : self.facebookId),
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     @"opt_out_facebook" : self.optOutFacebook ? @YES : @NO,
+#pragma clang diagnostic pop
     @"opt_out_idfa" : self.optOutIdfa ? @YES : @NO,
     @"opt_out_push_key" : self.optOutPushKey ? @YES : @NO
   };
@@ -18,7 +21,10 @@
   if (copy) {
     copy.email = [self.email copy];
     copy.facebookId = [self.facebookId copy];
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
     copy.optOutFacebook = self.optOutFacebook;
+#pragma clang diagnostic pop
     copy.optOutIdfa = self.optOutIdfa;
     copy.optOutPushKey = self.optOutPushKey;
   }
